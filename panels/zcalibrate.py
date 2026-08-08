dc8eb46709c514731db2eb481cfa50a9bf950523
edwardfang2018@outlook.com <edwardfang2018>
2026-08-08 17:49:30 +0800

    Fixed zcalibrate bug

--- panels/zcalibrate.py
+++ panels/zcalibrate.py
@@ -251,18 +251,18 @@ class Panel(ScreenPanel):
         self._screen._ws.api.gcode_script("G0 Z10 F300")
         self._screen._ws.api.gcode_script("G0 X0 Y0 F1000")
         self.bmc_points = self._printer.get_stat('bed_mesh', 'bmc_points')
-        if "Z_ENDSTOP_CALIBRATE" in command:
-            x_offset = y_offset = None
-            if self.probe:
-                if "x_offset" in self.probe:
-                    x_offset = float(self.probe['x_offset'])
-                if "y_offset" in self.probe:
-                    y_offset = float(self.probe['y_offset'])
-            if self.bmc_points[0][0] is not None and self.bmc_points[0][1] is not None:
-                if self._printer.config_section_exists("extruder1"):
-                    self._screen._ws.api.gcode_script(f'G0 X{self.bmc_points[0][0]} Y{self.bmc_points[0][1]} F1000')
-                else:
-                    self._screen._ws.api.gcode_script(f'G0 X{self.bmc_points[0][0] - x_offset} Y{self.bmc_points[0][1] - y_offset} F1000')
+        #if "Z_ENDSTOP_CALIBRATE" in command:
+        #    x_offset = y_offset = None
+        #    if self.probe:
+        #        if "x_offset" in self.probe:
+        #            x_offset = float(self.probe['x_offset'])
+        #        if "y_offset" in self.probe:
+        #            y_offset = float(self.probe['y_offset'])
+        #    if self.bmc_points[0][0] is not None and self.bmc_points[0][1] is not None:
+        #        if self._printer.config_section_exists("extruder1"):
+        #            self._screen._ws.api.gcode_script(f'G0 X{self.bmc_points[0][0]} Y{self.bmc_points[0][1]} F1000')
+        #        else:
+        #            self._screen._ws.api.gcode_script(f'G0 X{self.bmc_points[0][0] - x_offset} Y{self.bmc_points[0][1] - y_offset} F1000')
         self._screen._ws.api.gcode_script(command)
 
     def _move_to_position(self, x, y):
